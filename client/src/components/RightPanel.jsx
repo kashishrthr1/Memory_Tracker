498079444e22a362f96a5444fe8918d593e7f57d
AyJ <notayushjadon@gmail.com>
2026-01-11 21:39:57 +0530

    styling-fixes

--- client/src/components/RightPanel.jsx
+++ client/src/components/RightPanel.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
 import LoginForm from "./LoginForm";
 import RegisterForm from "./RegisterForm";
 
 function RightPanel({ mode }) {
+  const navigate = useNavigate();
+  
   return (
     <div className="right-panel">
+      <div className="mobile-back-button" onClick={() => navigate("/")}>
+        <IoArrowBack className="back-arrow" />
+      </div>
       {mode === "login" ? <LoginForm /> : <RegisterForm />}
     </div>
   );
